a55815ab5377535aea31ff8ed6d826312c1b5fed
MickMake <github@mickmake.com>
2022-08-23 16:49:10 +1000

    v1.0.3

--- defaults/const.go
+++ defaults/const.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName      = "GoWebcam"
-	BinaryVersion   = "1.0.1"
+	BinaryVersion   = "1.0.3"
 	SourceRepo      = "github.com/MickMake/" + BinaryName
 	BinaryRepo      = "github.com/MickMake/" + BinaryName
 
